836a0a7f3099d857ff28cac74839d38c88221d29
Luke Plaster <notatestuser@gmail.com>
2010-12-14 12:47:13 +0000

    Integrated the JGraphX view into the VisualisationDesktopPane and implemented transitive class cell insertion.

--- src/org/lukep/javavis/ui/VisualisationDesktopPane.java
+++ src/org/lukep/javavis/ui/VisualisationDesktopPane.java
@@ -4,6 +4,7 @@
  */
 package org.lukep.javavis.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.io.File;
 import java.util.Enumeration;
@@ -14,6 +15,8 @@ import javax.swing.tree.DefaultTreeModel;
 
 import org.lukep.javavis.visualisation.java.JavaSourceLoaderThread;
 
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
 import com.sun.tools.javac.code.Symbol.ClassSymbol;
 
 public class VisualisationDesktopPane extends StatefulWorkspacePane implements IProgramSourceObserver {
@@ -21,12 +24,22 @@ public class VisualisationDesktopPane extends StatefulWorkspacePane implements I
 	private JTree programTree;
 	private JScrollPane codeOverviewPanel;
 	private JTabbedPane tabbedPane;
+	private mxGraph graph;
+	
+	private int cellX = 250;
+	private int cellY = 100;
 
 	public VisualisationDesktopPane(IProgramStatusReporter statusTarget) throws Exception {
 		super(statusTarget);
 		
-		setBackground(Color.WHITE);
-		setLayout(null);
+		this.setBackground(Color.WHITE);
+		this.setLayout(null);
+		
+		graph = new mxGraph();
+		JPanel graphCanvas = new JPanel( new BorderLayout() );
+		graphCanvas.add(new mxGraphComponent(graph), BorderLayout.CENTER);
+		graphCanvas.setSize(1000, 666);
+		this.add(graphCanvas, DEFAULT_LAYER);
 		
 		JInternalFrame layeredPane = new JInternalFrame();
 		layeredPane.setTitle("Toolbox");
@@ -34,7 +47,7 @@ public class VisualisationDesktopPane extends StatefulWorkspacePane implements I
 		layeredPane.setResizable(true);
 		layeredPane.setBounds(11, 11, 200, 300);
 		layeredPane.setVisible(true);
-		this.add(layeredPane);
+		this.add(layeredPane, PALETTE_LAYER);
 		
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(0, 0, 81, 516);
@@ -82,15 +95,23 @@ public class VisualisationDesktopPane extends StatefulWorkspacePane implements I
 				DefaultMutableTreeNode element = (DefaultMutableTreeNode) rootNodeChildren.nextElement();
 				if (element.toString().equals(packageName)) { // check if existing package node exists
 					element.add( new DefaultMutableTreeNode( clazz.getSimpleName() ) ); // yes, add it to that one
+					addClass( clazz.getSimpleName().toString() ); // create cell graph
 					addedToExistingNode = true;
 				}
 			}
 			if (!addedToExistingNode) {
 				rootNode.add( newPackageNode ); // add new package node
 				newPackageNode.add( new DefaultMutableTreeNode( clazz.getSimpleName() ) ); // add class sub-node
+				addClass( clazz.getSimpleName().toString() ); // create cell graph
 			}
 		}
 		((DefaultTreeModel)programTree.getModel()).reload();
+	}
+	
+	private void addClass(String className) {
+		graph.insertVertex(null, null, className, cellX, cellY, 150, 80);
+		cellX += 50;
+		cellY += 50;
 	}
 
 }

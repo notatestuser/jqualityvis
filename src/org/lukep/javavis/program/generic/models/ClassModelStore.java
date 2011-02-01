6be517ad1811a571411c0894c9d9614b06302593
Luke Plaster <notatestuser@gmail.com>
2011-02-01 07:17:35 +0000

    Renamed ClassModelMap to ClassModelStore Added a static vector to ClassModelStore to maintain a list of known instances of the classes Implemented a static lookupClassGlobal method in ClassModelStore to facilitate the looking up of a ClassInfo object from a class's qualified name in all known instances of ClassModelStore

--- src/org/lukep/javavis/program/generic/models/ClassModelStore.java
+++ src/org/lukep/javavis/program/generic/models/ClassModelStore.java
@@ -7,20 +7,22 @@ package org.lukep.javavis.program.generic.models;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Observable;
+import java.util.Vector;
 import java.util.logging.Logger;
 
 import org.lukep.javavis.ui.IProgramSourceObserver;
-import org.lukep.javavis.util.JavaVisConstants;
 
-public class ClassModelMap extends Observable implements IProgramSourceObserver {
+public class ClassModelStore extends Observable implements IProgramSourceObserver {
 	
 	protected final static Logger log = Logger.getLogger("GenericClassModelMap");
 	
+	// keeps track of all instances of the ClassModelStore (for global class lookups)
+	protected final static Vector<ClassModelStore> instances = new Vector<ClassModelStore>();
+	
     protected Map<String, ClassInfo> classMap = new HashMap<String, ClassInfo>();
-    protected ClassInfo lastClass = null;
 
-    public ClassModelMap() {
-    	
+    public ClassModelStore() {
+    	instances.add(this);
     }
 
     public Map<String, ClassInfo> getClassMap() {
@@ -35,27 +37,35 @@ public class ClassModelMap extends Observable implements IProgramSourceObserver 
         classMap.put(className, classInfo);
         log.info("Added class: " + className);
     }
+    
+    public ClassInfo lookupClass(String qualifiedName) {
+    	if (classMap.size() > 0 && classMap.containsKey(qualifiedName))
+    		return classMap.get(qualifiedName);
+    	return null;
+    }
+    
+    public static ClassInfo lookupClassGlobal(String qualifiedName) {
+    	// check all instances of ClassModelStore that we know about
+    	if (instances.size() > 0) {
+    		ClassInfo clazz = null;
+    		for (ClassModelStore cms : instances) {
+    			clazz = cms.lookupClass(qualifiedName);
+    			if (clazz != null)
+    				return clazz;
+    		}
+    	}
+    	return null;
+    }
 
 	@Override
 	public void notifyFindClass(ClassInfo clazz) {
 		addClass(clazz.getQualifiedName(), clazz);
-		lastClass = clazz;
 	}
 
 	@Override
 	public void notifyFindMethod(MethodInfo method) {
-		if (lastClass != null) {
-			if (method.getName().equals(JavaVisConstants.DEFAULT_CONSTRUCTOR_NAME)) {
-				lastClass.setConstructorMethod(method);
-				log.info("Added constructor method to " + lastClass.getSimpleName());
-			} else {
-				lastClass.addMethod(method);
-				log.info("Added method to " + lastClass.getSimpleName()
-														+ ": " + method.getName());
-			}
-				
-			method.setParentClass(lastClass);
-		}
+		// TODO Auto-generated method stub
+		
 	}
     
 }

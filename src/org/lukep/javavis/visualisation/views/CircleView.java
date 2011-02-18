d18e24652cf521bf681e5c5a8e377e9bd10cbbdb
Luke Plaster <notatestuser@gmail.com>
2011-02-18 10:43:06 +0000

    Added two transitive Prefuse visualisations (ClassHierarchyTree and CircleView).

--- src/org/lukep/javavis/visualisation/views/CircleView.java
+++ src/org/lukep/javavis/visualisation/views/CircleView.java
@@ -1,24 +1,17 @@
 /*
- * ClassHierarchyView.java (JMetricVis)
+ * CircleView.java (JMetricVis)
  * Copyright 2011 Luke Plaster. All rights reserved.
  */
 package org.lukep.javavis.visualisation.views;
 
 import java.awt.Color;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 
 import org.lukep.javavis.metrics.IMeasurable;
-import org.lukep.javavis.program.generic.models.ClassModel;
 import org.lukep.javavis.program.generic.models.IGenericModelNode;
-import org.lukep.javavis.program.generic.models.PackageModel;
 import org.lukep.javavis.program.generic.models.ProgramModelStore;
-import org.lukep.javavis.program.generic.models.Relationship;
-import org.lukep.javavis.program.generic.models.Relationship.RelationshipType;
 import org.lukep.javavis.ui.swing.PrefuseWorkspacePane;
 import org.lukep.javavis.ui.swing.WorkspaceContext;
-import org.lukep.javavis.ui.swing.mxGraphWorkspacePane;
 
 import prefuse.Constants;
 import prefuse.Display;
@@ -27,7 +20,7 @@ import prefuse.action.ActionList;
 import prefuse.action.RepaintAction;
 import prefuse.action.assignment.ColorAction;
 import prefuse.action.assignment.DataColorAction;
-import prefuse.action.layout.graph.ForceDirectedLayout;
+import prefuse.action.layout.CircleLayout;
 import prefuse.activity.Activity;
 import prefuse.data.Graph;
 import prefuse.data.Node;
@@ -36,97 +29,7 @@ import prefuse.render.LabelRenderer;
 import prefuse.util.ColorLib;
 import prefuse.visual.VisualItem;
 
-import com.mxgraph.layout.mxCompactTreeLayout;
-import com.mxgraph.layout.mxGraphLayout;
-import com.mxgraph.model.mxCell;
-import com.mxgraph.model.mxIGraphModel;
-import com.mxgraph.swing.mxGraphComponent;
-import com.mxgraph.swing.util.mxMorphing;
-import com.mxgraph.util.mxConstants;
-import com.mxgraph.util.mxEvent;
-import com.mxgraph.util.mxEventObject;
-import com.mxgraph.util.mxEventSource.mxIEventListener;
-import com.mxgraph.view.mxGraph;
-
-public class ClassHierarchyView extends AbstractVisualisationView {
-
-	@Override
-	public void visit(mxGraphWorkspacePane workspace, final WorkspaceContext wspContext,
-			final mxGraphComponent graphComponent) {
-		
-		// start the mxGraph transaction and clear model
-		final mxGraph graph = graphComponent.getGraph();
-		final mxIGraphModel graphModel = graphComponent.getGraph().getModel();
-		graphModel.beginUpdate();
-		graph.selectAll();
-		graph.removeCells();
-		
-		// bind graph layout manager
-		mxGraphLayout graphLayout = new mxCompactTreeLayout(graph);
-		
-		// create package vertices
-		ProgramModelStore modelStore = wspContext.getModelStore();
-		HashMap<IGenericModelNode, mxCell> parentCellMap = 
-			new HashMap<IGenericModelNode, mxCell>(
-				modelStore.getPackageMap().size() + 10);
-		mxCell curCell;
-		
-		for (PackageModel pkg : modelStore.getPackageMap().values()) {
-			
-			curCell = (mxCell) graph.insertVertex(graph.getDefaultParent(), 
-					pkg.getQualifiedName(), pkg, 250, 100, 150, 30);
-			
-			IGenericModelNode parentPackage = pkg.getParent();
-			if (parentPackage != null
-					&& parentPackage instanceof PackageModel
-					&& parentCellMap.containsKey(parentPackage)) {
-				
-				// link to the parent package with an edge
-				graph.insertEdge(graph.getDefaultParent(), null, null, 
-						parentCellMap.get(parentPackage), curCell, 
-						mxConstants.STYLE_ENDARROW + "=" + mxConstants.ARROW_OPEN);
-			}
-			
-			parentCellMap.put(pkg, curCell);
-		}
-		
-		// create class vertices
-		for (ClassModel clazz : modelStore.getClassMap().values()) {
-			
-			curCell = (mxCell) graph.insertVertex(graph.getDefaultParent(), null, clazz, 20, 20, 150, 50);
-			
-			if (parentCellMap.containsKey(clazz.getParent())) {
-				
-				// link to the parent package
-				graph.insertEdge(graph.getDefaultParent(), null, null, 
-						parentCellMap.get(clazz.getParent()), curCell);
-			}
-			
-			if (clazz.getChildCount() > 0)
-				parentCellMap.put(clazz, curCell);
-		}
-		
-		// execute the layout and animation
-		graphModel.endUpdate();
-		graphModel.beginUpdate();
-		
-		try {
-			graphLayout.execute(graph.getDefaultParent());
-		} finally {
-			mxMorphing morph = new mxMorphing(graphComponent, 20, 1.2, 20);
-			
-			morph.addListener(mxEvent.DONE, new mxIEventListener() {
-				
-				@Override
-				public void invoke(Object sender, mxEventObject evt) {
-					graphModel.endUpdate();
-				}
-				
-			});
-			
-			morph.startAnimation();
-		}
-	}
+public class CircleView extends AbstractVisualisationView {
 
 	@Override
 	public void visit(PrefuseWorkspacePane workspace, WorkspaceContext wspContext,
@@ -147,47 +50,12 @@ public class ClassHierarchyView extends AbstractVisualisationView {
 				modelStore.getPackageMap().size() + 10);
 		Node curNode;
 		
-		for (PackageModel pkg : modelStore.getPackageMap().values()) {
-			
-			// add the package node
-			curNode = graph.addNode();
-			curNode.setString("type", pkg.getModelTypeName());
-			curNode.setString("name", pkg.getSimpleName());
-			curNode.set("model", pkg);
-			
-			// link to the parent with an edge
-			IGenericModelNode parentPackage = pkg.getParent();
-			if (parentPackage != null
-					&& parentPackage instanceof PackageModel
-					&& parentNodeMap.containsKey(parentPackage)) {
-				
-				// link to the parent package with an edge
-				graph.addEdge(parentNodeMap.get(parentPackage), curNode);
-			}
-			
-			parentNodeMap.put(pkg, curNode);
-		}
-		
-		// create class and method nodes
-		Queue<IGenericModelNode> modelQueue = 
-			new LinkedList<IGenericModelNode>(modelStore.getClassMap().values());
-		IGenericModelNode model;
-		
-		while (modelQueue.peek() != null) {
-			model = modelQueue.remove();
-			
+		// create class nodes
+		for (IGenericModelNode model : modelStore.getClassMap().values()) {
 			curNode = graph.addNode();
 			curNode.setString("type", model.getModelTypeName());
 			curNode.setString("name", model.getSimpleName());
 			curNode.set("model", model);
-			
-			// add children to iterModels
-			if (model.getChildCount() > 0) {
-				for (Relationship r : model.getChildren())
-					if (r.getRelationshipType() == RelationshipType.ENCLOSED_IN)
-						modelQueue.add(r.getTarget());
-				parentNodeMap.put(model, curNode);
-			}
 			
 			// link to the parent package
 			if (parentNodeMap.containsKey(model.getParent())) {
@@ -252,7 +120,7 @@ public class ClassHierarchyView extends AbstractVisualisationView {
         // create an action list with an animated layout
         ActionList layout = new ActionList(Activity.INFINITY);
         //layout.add(new ForceDirectedLayout("graph"));
-        layout.add(new ForceDirectedLayout("graph"));
+        layout.add(new CircleLayout("graph"));
         layout.add(new RepaintAction());
         
         // add the actions to the visualisation
@@ -268,5 +136,5 @@ public class ClassHierarchyView extends AbstractVisualisationView {
 		vis.run("colour");
 		vis.run("layout");
 	}
-
+	
 }
--- src/org/lukep/javavis/visualisation/views/ClassHierarchyTree.java
+++ src/org/lukep/javavis/visualisation/views/ClassHierarchyTree.java
@@ -1,5 +1,5 @@
 /*
- * ClassHierarchyView.java (JMetricVis)
+ * ClassHierarchyTree.java (JMetricVis)
  * Copyright 2011 Luke Plaster. All rights reserved.
  */
 package org.lukep.javavis.visualisation.views;
@@ -10,7 +10,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 import org.lukep.javavis.metrics.IMeasurable;
-import org.lukep.javavis.program.generic.models.ClassModel;
 import org.lukep.javavis.program.generic.models.IGenericModelNode;
 import org.lukep.javavis.program.generic.models.PackageModel;
 import org.lukep.javavis.program.generic.models.ProgramModelStore;
@@ -18,7 +17,6 @@ import org.lukep.javavis.program.generic.models.Relationship;
 import org.lukep.javavis.program.generic.models.Relationship.RelationshipType;
 import org.lukep.javavis.ui.swing.PrefuseWorkspacePane;
 import org.lukep.javavis.ui.swing.WorkspaceContext;
-import org.lukep.javavis.ui.swing.mxGraphWorkspacePane;
 
 import prefuse.Constants;
 import prefuse.Display;
@@ -27,7 +25,7 @@ import prefuse.action.ActionList;
 import prefuse.action.RepaintAction;
 import prefuse.action.assignment.ColorAction;
 import prefuse.action.assignment.DataColorAction;
-import prefuse.action.layout.graph.ForceDirectedLayout;
+import prefuse.action.layout.graph.NodeLinkTreeLayout;
 import prefuse.activity.Activity;
 import prefuse.data.Graph;
 import prefuse.data.Node;
@@ -36,102 +34,12 @@ import prefuse.render.LabelRenderer;
 import prefuse.util.ColorLib;
 import prefuse.visual.VisualItem;
 
-import com.mxgraph.layout.mxCompactTreeLayout;
-import com.mxgraph.layout.mxGraphLayout;
-import com.mxgraph.model.mxCell;
-import com.mxgraph.model.mxIGraphModel;
-import com.mxgraph.swing.mxGraphComponent;
-import com.mxgraph.swing.util.mxMorphing;
-import com.mxgraph.util.mxConstants;
-import com.mxgraph.util.mxEvent;
-import com.mxgraph.util.mxEventObject;
-import com.mxgraph.util.mxEventSource.mxIEventListener;
-import com.mxgraph.view.mxGraph;
-
-public class ClassHierarchyView extends AbstractVisualisationView {
+public class ClassHierarchyTree extends AbstractVisualisationView {
 
 	@Override
-	public void visit(mxGraphWorkspacePane workspace, final WorkspaceContext wspContext,
-			final mxGraphComponent graphComponent) {
-		
-		// start the mxGraph transaction and clear model
-		final mxGraph graph = graphComponent.getGraph();
-		final mxIGraphModel graphModel = graphComponent.getGraph().getModel();
-		graphModel.beginUpdate();
-		graph.selectAll();
-		graph.removeCells();
-		
-		// bind graph layout manager
-		mxGraphLayout graphLayout = new mxCompactTreeLayout(graph);
-		
-		// create package vertices
-		ProgramModelStore modelStore = wspContext.getModelStore();
-		HashMap<IGenericModelNode, mxCell> parentCellMap = 
-			new HashMap<IGenericModelNode, mxCell>(
-				modelStore.getPackageMap().size() + 10);
-		mxCell curCell;
-		
-		for (PackageModel pkg : modelStore.getPackageMap().values()) {
-			
-			curCell = (mxCell) graph.insertVertex(graph.getDefaultParent(), 
-					pkg.getQualifiedName(), pkg, 250, 100, 150, 30);
-			
-			IGenericModelNode parentPackage = pkg.getParent();
-			if (parentPackage != null
-					&& parentPackage instanceof PackageModel
-					&& parentCellMap.containsKey(parentPackage)) {
-				
-				// link to the parent package with an edge
-				graph.insertEdge(graph.getDefaultParent(), null, null, 
-						parentCellMap.get(parentPackage), curCell, 
-						mxConstants.STYLE_ENDARROW + "=" + mxConstants.ARROW_OPEN);
-			}
-			
-			parentCellMap.put(pkg, curCell);
-		}
-		
-		// create class vertices
-		for (ClassModel clazz : modelStore.getClassMap().values()) {
-			
-			curCell = (mxCell) graph.insertVertex(graph.getDefaultParent(), null, clazz, 20, 20, 150, 50);
-			
-			if (parentCellMap.containsKey(clazz.getParent())) {
-				
-				// link to the parent package
-				graph.insertEdge(graph.getDefaultParent(), null, null, 
-						parentCellMap.get(clazz.getParent()), curCell);
-			}
-			
-			if (clazz.getChildCount() > 0)
-				parentCellMap.put(clazz, curCell);
-		}
-		
-		// execute the layout and animation
-		graphModel.endUpdate();
-		graphModel.beginUpdate();
-		
-		try {
-			graphLayout.execute(graph.getDefaultParent());
-		} finally {
-			mxMorphing morph = new mxMorphing(graphComponent, 20, 1.2, 20);
-			
-			morph.addListener(mxEvent.DONE, new mxIEventListener() {
-				
-				@Override
-				public void invoke(Object sender, mxEventObject evt) {
-					graphModel.endUpdate();
-				}
-				
-			});
-			
-			morph.startAnimation();
-		}
-	}
+	public void visit(PrefuseWorkspacePane workspace,
+			WorkspaceContext wspContext, Display display) {
 
-	@Override
-	public void visit(PrefuseWorkspacePane workspace, WorkspaceContext wspContext,
-			Display display) {
-		
 		// -- 1. load the data ------------------------------------------------
 
 		Graph graph = new Graph();
@@ -252,7 +160,7 @@ public class ClassHierarchyView extends AbstractVisualisationView {
         // create an action list with an animated layout
         ActionList layout = new ActionList(Activity.INFINITY);
         //layout.add(new ForceDirectedLayout("graph"));
-        layout.add(new ForceDirectedLayout("graph"));
+        layout.add(new NodeLinkTreeLayout("graph"));
         layout.add(new RepaintAction());
         
         // add the actions to the visualisation

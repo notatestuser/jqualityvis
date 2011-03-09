1c16226e21cafb11fa057427020728191d78f763
Luke Plaster <notatestuser@gmail.com>
2011-03-09 02:21:48 +0000

    oops

---
+++ src/org/lukep/javavis/metrics/algorithms/qmood/DirectCouplingVisitor.java
@@ -0,0 +1,21 @@
+/*
+ * DirectCouplingVisitor.java (JMetricVis)
+ * Copyright 2011 Luke Plaster. All rights reserved.
+ */
+package org.lukep.javavis.metrics.algorithms.qmood;
+
+import org.lukep.javavis.metrics.MetricAttribute;
+import org.lukep.javavis.metrics.MetricMeasurement;
+import org.lukep.javavis.metrics.algorithms.AbstractMeasurableVisitor;
+import org.lukep.javavis.program.generic.models.ClassModel;
+import org.lukep.javavis.program.generic.models.MethodModel;
+
+public class DirectCouplingVisitor extends AbstractMeasurableVisitor {
+
+	@Override
+	public MetricMeasurement visit(MetricAttribute metric, ClassModel clazz) {
+		
+		Set<>
+	}
+
+}
